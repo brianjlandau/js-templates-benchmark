9f60ec4f03b975b0e2c4f4b7c216bd99763d3d7b
Brian Landau <brianjlandau@gmail.com>
2009-11-30 15:16:21 -0500

    Fix spacing.

--- jquery.benchmark.js
+++ jquery.benchmark.js
@@ -2,17 +2,17 @@
 // http://www.quirksmode.org/blog/archives/2009/08/when_to_read_ou.html
 (function($){
 $.benchmark = function(times, result_selector, func){
-	var startTime = new Date().getTime();
+  var startTime = new Date().getTime();
   
   while (times != 0){
     func();
     times--;
   }
 
-	setTimeout(function () {
-		var endTime = new Date().getTime();
-		var result = (endTime-startTime)/1000;
-		$(result_selector).html(result);
-	},10);
+  setTimeout(function () {
+    var endTime = new Date().getTime();
+    var result = (endTime-startTime)/1000;
+    $(result_selector).html(result);
+  },10);
 };
 })(jQuery);
